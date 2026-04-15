5b69248026d4641d746a78a426740d7c9da855a7
Daniel <boluwatifedaniel705@gmail.com>
2026-04-15 16:30:37 +0000

    final (1)

--- schemas.py
+++ schemas.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 
+# INPUT MODEL (request body)
+class ProfileRequest(BaseModel):
+    name: str
+
+
+# OUTPUT MODEL (data returned inside "data")
 class ProfileData(BaseModel):
     id: str
     name: str
@@ -12,9 +18,8 @@ class ProfileData(BaseModel):
     country_probability: float
     created_at: str
 
-  
 
-
+# FULL RESPONSE MODEL
 class ProfileResponse(BaseModel):
     status: str
     data: ProfileData
