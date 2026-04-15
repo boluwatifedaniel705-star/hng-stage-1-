98efa272fdd4503e04d0d287c4d251106996ff3d
Daniel <boluwatifedaniel705@gmail.com>
2026-04-15 05:47:27 +0000

    done

--- schemas.py
+++ schemas.py
@@ -1,7 +1,19 @@
 from pydantic import BaseModel
 
+class ProfileData(BaseModel):
+    id: str
+    name: str
+    gender: str
+    gender_probability: float
+    sample_size: int
+    age: int
+    age_group: str
+    country_id: str
+    country_probability: float
+    created_at: str
+
+
 class ProfileResponse(BaseModel):
     status: str
-    data: dict
-
+    data: ProfileData
    
